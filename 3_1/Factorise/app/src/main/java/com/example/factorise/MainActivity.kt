e9f3acd39b50c20fefbe60bba5bb3b5bb65ad5d7
dgomilko <digomilko@gmail.com>
2021-03-22 13:44:41 +0200

    additional task

--- 3_1/Factorise/app/src/main/java/com/example/factorise/MainActivity.kt
+++ 3_1/Factorise/app/src/main/java/com/example/factorise/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
     private lateinit var button: Button
     private lateinit var result: TextView
+    private lateinit var iterations: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         input = findViewById(R.id.editTextNumber)
         button = findViewById(R.id.factoriseBtn)
         result = findViewById(R.id.textView)
+        iterations = findViewById(R.id.textViewCounter)
 
         button.setOnClickListener { handleBtnClick() }
     }
@@ -31,25 +33,32 @@ class MainActivity : AppCompatActivity() {
         output = if (text.isEmpty()) "Input area is empty"
         else {
             val n = text.toInt()
+
             val res = fermatFactorise(n)
-            if (res !== null) "Result: ${res.first}, ${res.second}" else "Invalid input data"
+            if (res !== null) {
+                val iter = "Iterations count: ${res[2]}"
+                iterations.text = iter
+                "Result: ${res[0]}, ${res[1]}"
+            } else "Invalid input data"
         }
         result.text = output
     }
 
-    private fun fermatFactorise(n: Int): Pair<Int, Int>? {
+    private fun fermatFactorise(n: Int): List<Int>? {
         if (n <= 0) return null
-        if (n % 2 == 0) return Pair(2, n / 2)
+        if (n % 2 == 0) return listOf(2, n / 2, 1)
 
+        var iterationsCount = 0
         var x = kotlin.math.ceil(kotlin.math.sqrt(n.toDouble()))
         var y = x.pow(2) - n
 
         while (kotlin.math.sqrt(y) != kotlin.math.ceil(kotlin.math.sqrt(y))) {
+            iterationsCount++
             x++
             y = x.pow(2) - n
         }
 
         val ySqrt = kotlin.math.sqrt(y)
-        return Pair((x + ySqrt).toInt(), (x - ySqrt).toInt())
+        return listOf((x + ySqrt).toInt(), (x - ySqrt).toInt(), iterationsCount)
     }
 }
